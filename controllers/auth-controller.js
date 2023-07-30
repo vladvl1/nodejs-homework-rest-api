d38ce8b980fa05a1a9c657098b3762b8e3b519ed
Vladyslav Hrytsenko <unknownghost84.vg@gmail.com>
2023-07-30 20:50:35 +0300

    fix

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -69,7 +69,7 @@ const logout = async(req, res) => {
     const {_id} = req.user;
     await User.findByIdAndUpdate(_id, {token: ""});
     
-    res.status(204);
+    res.status(204).json();
 }
 
 export default {
