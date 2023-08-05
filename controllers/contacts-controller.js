cbc96019bacc8d2e15b1d60e41e09668597ea905
Vladyslav Hrytsenko <unknownghost84.vg@gmail.com>
2023-08-05 20:42:21 +0300

    0

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -27,7 +27,7 @@ const add = async (req, res) => {
     const {_id:owner} = req.user;
     const {path: oldPath, filename} = req.file;
     const newPath = path.join(avatarPath, filename);
-    fs.rename(oldPath, newPath);
+    await fs.rename(oldPath, newPath);
     const avatar = path.join("avatars", filename);
     const result = await Contact.create({...req.body, avatar, owner});
     res.status(201).json(result);
