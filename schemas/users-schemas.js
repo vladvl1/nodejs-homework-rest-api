d5840812ce8bc7e39cbaf07cd93aa94e76bf483b
Vladyslav Hrytsenko <unknownghost84.vg@gmail.com>
2023-08-16 21:01:51 +0300

    try 1

--- schemas/users-schemas.js
+++ schemas/users-schemas.js
@@ -11,8 +11,14 @@ const userLoginSchema = Joi.object({
     email: Joi.string().pattern(emailRegexp).required(),
     password: Joi.string().min(6).required(),
 })
+const userEmailSchema = Joi.object({
+    email: Joi.string().pattern(emailRegexp).required().messages({
+        "any.required":`missing required field email`,
+    })
+})
 
 export default {
     userRegisterSchema,
     userLoginSchema,
+    userEmailSchema,
 }
